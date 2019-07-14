2e18413bd14dff34f99efa48a570e11f4a211425
Rahat <rahat392@gmail.com>
2019-07-14 15:35:49 +0600

    Adding Image and fonts.

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'image_banner.dart';
+import 'text_section.dart';
 
 class LocationDetail extends StatelessWidget {
   @override
@@ -8,7 +10,16 @@ class LocationDetail extends StatelessWidget {
       appBar: AppBar(
         title: Text('Location Details'),
       ),
-      body: Text('Hello'),
+      body:Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          ImageBanner('assets/images/image_1.jpg'),
+          TextSection('Summary','something1'),
+          TextSection('Summary','something1'),
+          TextSection('Summary','something1'),
+        ],
+      ) 
     );
   }
   
