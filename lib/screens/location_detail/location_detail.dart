fca9018afade2802f9edc3ec1abbb7db4e4f7e75
Rahat <rahat392@gmail.com>
2019-07-14 12:36:53 +0600

    Added location details widget

---
+++ lib/screens/location_detail/location_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LocationDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Location Details'),
+      ),
+      body: Text('Hello'),
+    );
+  }
+  
+}
